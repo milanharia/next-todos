079e40e8758ac12afcd88471c42683928aba8c1f
Milan Haria <milan.haria@morrisonsplc.co.uk>
2024-11-02 15:21:23 +0000

    update todo schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,5 +1,11 @@
 import { relations } from "drizzle-orm";
-import { boolean, pgTableCreator, serial, varchar } from "drizzle-orm/pg-core";
+import {
+  boolean,
+  pgTableCreator,
+  serial,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
 
 export const createTable = pgTableCreator((name) => `next_todos_${name}`);
 
@@ -17,10 +23,14 @@ export const usersRelations = relations(users, ({ many }) => ({
 
 export const todos = createTable("todos", {
   id: serial("id").primaryKey(),
-  authorId: varchar("author_id").references(() => users.id),
+  authorId: varchar("author_id")
+    .references(() => users.id)
+    .notNull(),
   title: varchar("title").notNull(),
   content: varchar("content").notNull(),
   completed: boolean("completed").notNull().default(false),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at"),
 });
 
 export const todosRelations = relations(todos, ({ one }) => ({
